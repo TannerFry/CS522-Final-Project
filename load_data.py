8a5795be26bbdcb2b0beb49ed0156422ce3d9ba4
TannerFry <tfry2@vols.utk.edu>
2020-11-03 17:37:22 -0500

    adding files for loading/preprocessing data and for random forest model

---
+++ load_data.py
@@ -0,0 +1,48 @@
+import csv
+import string
+import re
+import nltk
+from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+#only to run once to download stopword data and wordnet for lemmatizing
+#nltk.download('stopwords')
+#nltk.download('wordnet')
+
+
+#read data from file
+def read_data(file):
+    data = []
+    first = True
+    with open(file, "r", encoding='utf-8') as f:
+        reader = csv.reader(f)
+        for line in reader:
+            if first != True:
+                data.append(line)
+            first = False
+    return data
+
+#preprocess data
+def clean_data(text):
+    ps = nltk.PorterStemmer()
+    sw = stopwords.words('english')
+    text = "".join([word.lower() for word in text if word not in string.punctuation])
+    tokens = re.split('\W+', text)
+    text = [ps.stem(word) for word in tokens if word not in sw]
+    return text
+
+#vectorize data
+def vectorize_data(training_data, testing_data):
+    train_tweets_tokenized = []
+    for row in training_data:
+        train_tweets_tokenized.append(row[3])
+    test_tweets_tokenized = []
+    for row in testing_data:
+        test_tweets_tokenized.append(row[3])
+
+    tfidf_vect = TfidfVectorizer(analyzer=clean_data)
+    tfidf_vect_fit = tfidf_vect.fit(train_tweets_tokenized)
+    X_train = tfidf_vect_fit.transform(train_tweets_tokenized)
+    X_test = tfidf_vect_fit.transform(test_tweets_tokenized)
+
+    return X_train, X_test
---
+++ rf.py
@@ -0,0 +1,36 @@
+import load_data
+import pandas as pd
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import precision_recall_fscore_support as score
+from sklearn.metrics import accuracy_score as acs
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.metrics import confusion_matrix
+
+#load data
+training_data = load_data.read_data("train.csv")
+testing_data = load_data.read_data("test.csv")
+testing_labels = load_data.read_data("submission.csv")
+X_train, X_test = load_data.vectorize_data(training_data, testing_data)
+
+Y_train = np.array(training_data)[:, -1]
+Y_test = np.array(testing_labels)[:, -1]
+
+print("Building Random Forest Model")
+rf = RandomForestClassifier(n_estimators=150, max_depth=None, n_jobs=-1)
+rf_model = rf.fit(X_train, Y_train)
+Y_pred = rf.predict(X_test)
+
+precision, recall, fscore, train_support = score(Y_test, Y_pred, pos_label='1', average='binary')
+print('Precision: {} / Recall: {} / F1-Score: {} / Accuracy: {}'.format(
+    round(precision, 3), round(recall, 3), round(fscore,3), round(acs(Y_test,Y_pred), 3)))
+
+cm = confusion_matrix(Y_test, Y_pred)
+class_label = ["0", "1"]
+df_cm = pd.DataFrame(cm, index=class_label,columns=class_label)
+sns.heatmap(df_cm, annot=True, fmt='d')
+plt.title("Confusion Matrix")
+plt.xlabel("Predicted Label")
+plt.ylabel("True Label")
+plt.show()
